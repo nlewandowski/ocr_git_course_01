3e2931bff81068a2fb6168f7219b4a6b7447a5e9
nlewandowski <nicolas.lewandowski@gmail.com>
2017-10-12 13:20:46 +0200

    Variabilisation des noms de fichier de sortie

--- s2script.py
+++ s2script.py
@@ -10,10 +10,14 @@ dropOffPos = list()
 pickUpPos = list()
 
 inputfile = '/home/administrateur/Workspace/results-20170726-154612.csv'
+driverfile= '/home/administrateur/Workspace/drivers.csv'
+dropofffile='/home/administrateur/Workspace/dropoff.csv'
+pickupfile= '/home/administrateur/Workspace/pickup.csv'
+
 with open(inputfile, 'r') as csvfile:
    posreader=csv.reader(csvfile, delimiter=',')
    for row in posreader:
-     #print(row)
+
      mDriverPos = s2.LatLng.from_degrees(float(row[0]), float(row[1]))
      mDropOffPos = s2.LatLng.from_degrees(float(row[2]), float(row[3]))
      mPickUpPos = s2.LatLng.from_degrees(float(row[4]), float(row[5]))
@@ -21,15 +25,15 @@ with open(inputfile, 'r') as csvfile:
      dropOffPos.append([str(row[2]), str(row[3]), str(getParentLvl13(s2.Cell.from_lat_lng(mDropOffPos))).split(' ')[1][:8]])
      pickUpPos.append([str(row[4]), str(row[5]), str(getParentLvl13(s2.Cell.from_lat_lng(mPickUpPos))).split(' ')[1][:8]])
 
-with open('/home/administrateur/Workspace/drivers.csv', 'w') as driverFile:
+with open(driverfile, 'w') as driverFile:
   driverWriter=csv.writer(driverFile, delimiter=',')
   driverWriter.writerows(driverPos)
 
-with open('/home/administrateur/Workspace/dropoff.csv', 'w') as dropoffFile:
+with open(dropofffile, 'w') as dropoffFile:
   dropoffWriter=csv.writer(dropoffFile, delimiter=',')
   dropoffWriter.writerows(dropOffPos)
 
-with open('/home/administrateur/Workspace/pickup.csv', 'w') as pickupFile:
+with open(pickupfile, 'w') as pickupFile:
   pickupWriter=csv.writer(pickupFile, delimiter=',')
   pickupWriter.writerows(pickUpPos)
 
